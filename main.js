2e76b6ea3c0bc38ea5f41d8496218af81d42a477
Jesús Gónzalez <jesusgonzalez@MacBook-Pro-de-Jesus.local>
2024-07-10 11:38:23 -0600

    listar usuario filtrado

--- main.js
+++ main.js
@@ -18,6 +18,13 @@ app.use("/users",(req,res) => {
     res.json(users);
 });
 
+app.use("/users/:id",(req,res) =>{
+    const {id} = req.params;
+    const user = users.find(user => user.id  == id);
+
+    res.json(user);
+});
+
 app.listen(PORT,() => {
     console.log("Servidor corriendo en el puerto"+PORT);
 });
