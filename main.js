bcca1ccf7834ce627aa95e1afca83cf9002318a7
Jesús Gónzalez <jesusgonzalez@MacBook-Pro-de-Jesus.local>
2024-07-10 11:33:27 -0600

    listado de usuarios

--- main.js
+++ main.js
@@ -2,6 +2,22 @@ const app = require('expreess');
 
 const PORT = 3000;
 
+
+const users = [
+    {
+        id:1,
+        username:"jesusg"
+    },
+    {
+        id:2,
+        username:"panchito"
+    }
+];
+
+app.use("/users",(req,res) => {
+    res.json(users);
+});
+
 app.listen(PORT,() => {
     console.log("Servidor corriendo en el puerto"+PORT);
 });
